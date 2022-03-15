33097eabc316a60dd74537d74d96a67ba5e769d5
Leo <Leo@DESKTOP-K9P38CG>
2022-03-15 09:33:55 -0300

    Primeiro Commit

---
+++ src/view/Main.java
@@ -0,0 +1,16 @@
+package view;
+
+import controller.Matrix;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 3; i++) {
+			Thread t = new Matrix(i);
+			t.start();
+		}
+		
+	}
+
+}
